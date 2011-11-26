68dac7b08bd24e4a8757f7ce2fb05119a1974c38
Nell Shamrell <nshamrell@bluebox.net>
2011-11-26 10:15:47 -0800

    adding in a class to allow the program to make twitter posts

---
+++ lib/make_posts.rb
@@ -0,0 +1,23 @@
+require 'twitter'
+require_relative '../credentials'
+
+class MakePosts
+  def post_status(status)
+    Twitter.configure do |config|
+      credentials = Credentials.new
+      config.consumer_key = credentials.consumer_key
+      config.consumer_secret = credentials.consumer_secret
+      config.oauth_token = credentials.oauth_token
+      config.oauth_token_secret = credentials.oauth_token_secret
+    end
+
+     Twitter.update(status)
+
+  end
+end
+
+make_posts = MakePosts.new
+make_posts.post_status("Trying passing the status from a method")  
+  
+
+ 
